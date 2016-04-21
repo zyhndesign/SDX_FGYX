9105a2dcd5d00f6a35ddde22ac646776b456a335
codeliling <maximuslee@126.com>
2016-04-21 09:33:53 +0800

    fix bugs

--- src/main/java/com/cidic/sdx/HomeController.java
+++ src/main/java/com/cidic/sdx/HomeController.java
@@ -4,12 +4,21 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cidic.sdx.exception.SdxException;
+import com.cidic.sdx.model.ResultModel;
+import com.qiniu.util.Auth;
+import com.qiniu.util.StringMap;
 
 /**
  * Handles requests for the application home page.
@@ -18,6 +27,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	private ResultModel resultModel = null;
+	
+	@ExceptionHandler(SdxException.class)
+	public @ResponseBody ResultModel handleCustomException(SdxException ex) {
+		ResultModel resultModel = new ResultModel();
+		resultModel.setResultCode(ex.getErrCode());
+		resultModel.setMessage(ex.getErrMsg());
+		return resultModel;
+	}
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -34,6 +53,30 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	
+	@RequestMapping(value = "/getUploadKey", method = RequestMethod.GET)
+	@ResponseBody
+	public ResultModel getUploadKey(HttpServletRequest request){
+			
+		final String ACCESS_KEY = "Q-DeiayZfPqA0WDSOGSf-ekk345VrzuZa_6oBrX_";
+		final String SECRET_KEY = "fIiGiRr3pFmHOmBDR2Md1hTCqpMMBcE_gvZYMzwD";
+		final String bucketname = "design-course";
+		try{
+			StringMap strMap = new StringMap().putNotNull("returnBody", "{\"key\": $(key), \"hash\": $(etag), \"w\": $(imageInfo.width), \"h\": $(imageInfo.height)}");
+			Auth auth = Auth.create(ACCESS_KEY, SECRET_KEY);
+			
+			String token = auth.uploadToken(bucketname,null,3600,strMap);
+			
+			resultModel = new ResultModel();
+			resultModel.setResultCode(200);
+			resultModel.setUptoken(token);
+		}
+		catch(Exception e){
+			throw new SdxException(500, "获取数据失败！");
+		}
+		
+		return resultModel;
 	}
 	
 }
